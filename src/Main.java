3bbfc90f7d7cbf16fa6a01fe8c15ee311861e663
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-08-29 21:27:36 -0300

    sl2

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,29 @@ public class Main {
         ArrayList<Integer> idades = new ArrayList<Integer>();
 
         idades.add(22);
-                idades.add(445);
-idades.remove(0);
-idades.get(1);
-idades.size();
+        idades.add(445);
+        idades.remove(0);
+        idades.get(1);
+        idades.size();
+
+        double idade = 22.50;
+        if (idade > 22.60) {
+            System.out.println("maior");
+        } else if (idade == 22.50) {
+            System.out.println("igual");
+        } else {
+            System.out.println("menor");
+        }
+
+        for(var i = 0; i < idade; i++){
+            System.out.println("oi");
+        }
+
+        int id = 1;
+        while( id < idade) {
+        id++;
+        }
 
 
-double idade = 22.50;
-if ( idade > 22.60) {
-    System.out.println("maior");
-} else if (idade == 22.50)  {
-    System.out.println("igual");
-} else {
-    System.out.println("menor");
-}
     }
 }
