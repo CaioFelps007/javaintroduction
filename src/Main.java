3fdd1ede05a0c9ffe134e060c5cf71525652c5cf
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-08-29 21:16:27 -0300

    sla

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> idades = new ArrayList<Integer>();
 
+        idades.add(22);
+                idades.add(445);
+idades.remove(0);
+idades.get(1);
+idades.size();
+
+
+double idade = 22.50;
+if ( idade > 22.60) {
+    System.out.println("maior");
+} else if (idade == 22.50)  {
+    System.out.println("igual");
+} else {
+    System.out.println("menor");
+}
     }
 }
